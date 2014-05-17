bf7559d52b4ae38cc22404f56fa35e0d77e5084f
scheakur <scheakur@gmail.com>
2014-05-18 03:31:22 +0900

    First implementation

---
+++ data/htb.js
@@ -0,0 +1,23 @@
+var container = document.querySelector('.htb');
+
+var container = new Vue({
+  el: container,
+  data: {
+    bookmarks: [],
+  },
+});
+
+
+self.port.on('show', function(url, data) {
+  container.$data.url = url;
+
+  if (data === null) {
+    container.$data.num = 0;
+    container.$data.bookmarks = [];
+  } else {
+    container.$data.num = data.bookmarks.length;
+    container.$data.bookmarks = data.bookmarks.filter(function(b) {
+      return b.comment.length > 0;
+    });
+  }
+});
