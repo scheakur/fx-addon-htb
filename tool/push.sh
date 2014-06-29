c9d2c6af71dc964695007050f7f34f82f902427d
scheakur <scheakur@gmail.com>
2014-06-29 17:34:31 +0900

    Update script to push xpi

--- tool/push.sh
+++ tool/push.sh
@@ -3,7 +3,7 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-git checkout gh-pages
+git checkout -b gh-pages origin/gh-pages
 git add -A
 git commit -m 'Update package by Travis CI'
-[ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git 2> /dev/null
+[ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git gh-pages 2> /dev/null
