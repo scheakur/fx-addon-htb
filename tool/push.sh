73f1c20f8d1bf3600e6b72f57ed6b2406b54a561
scheakur <scheakur@gmail.com>
2014-06-29 17:46:29 +0900

    Test

--- tool/push.sh
+++ tool/push.sh
@@ -3,7 +3,5 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-git checkout -b gh-pages remotes/origin/gh-pages
-git add -A
-git commit -m 'Update package by Travis CI'
-[ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git gh-pages 2> /dev/null
+git branch -a
+
