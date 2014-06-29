d01c6db6132908a5306a0139bd116fad347e07df
scheakur <scheakur@gmail.com>
2014-06-29 18:38:32 +0900

    Update after script

--- tool/push.sh
+++ tool/push.sh
@@ -3,15 +3,11 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-git status
-git branch -a
-git fetch --all
-git status
-git branch -a
-
-git checkout -b gh-pages remotes/origin/gh-pages
-git status
-git branch -a
+cd build
+git clone --quiet https://github.com/scheakur/fx-addon-htb.git
+cd fx-addon-htb
+git branch -b gh-pages origin/gh-pages
+cp ../../*.xpi ./
 git add -A
-git commit -m 'Update package by Travis CI'
-[ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git gh-pages 2> /dev/null
+git commit -m 'Update package by Travis CI [$TRAVIS_BUID_NUMBER]'
+git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git gh-pages 2> /dev/null
