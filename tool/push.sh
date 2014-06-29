5c4d2704a2a4a5bcd32a11c080864acfd7e65809
scheakur <scheakur@gmail.com>
2014-06-29 17:48:43 +0900

    Fetch all branches before checkout

--- tool/push.sh
+++ tool/push.sh
@@ -3,5 +3,9 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-git branch -a
+git fetch --all
 
+git checkout -b gh-pages remotes/origin/gh-pages
+git add -A
+git commit -m 'Update package by Travis CI'
+[ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/scheakur/fx-addon-htb.git gh-pages 2> /dev/null
