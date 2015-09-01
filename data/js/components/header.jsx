40f88a8ef0fa66f3e50cf89fe726fc8e83168914
scheakur <scheakur@gmail.com>
2015-09-02 01:57:27 +0900

    Refactoring

--- data/js/components/header.jsx
+++ data/js/components/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export default class Header extends React.Component {
+function hide() {
+  self.port.emit('hide');
+}
 
-  hide() {
-    self.port.emit('hide');
-  }
+
+export default class Header extends React.Component {
 
   render() {
     return (
-      <a className="info" target="_blank" href={this.props.hatenaUrl} onClick={this.hide}>
+      <a className="info" target="_blank" href={this.props.hatenaUrl} onClick={hide}>
         <span className="symbol">B!</span>
         <span className="num">{this.props.num}</span>
         <span className="url">{this.props.url}</span>
